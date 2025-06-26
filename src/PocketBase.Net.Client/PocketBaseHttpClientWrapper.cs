b5dbd24bba1c3b085c6af4e922c1bfacd8681616
pBouillon <pro.pierre.bouillon@protonmail.com>
2025-06-26 13:38:09 +0200

    chore(client): centralize the http request logic

--- src/PocketBase.Net.Client/PocketBaseHttpClientWrapper.cs
+++ src/PocketBase.Net.Client/PocketBaseHttpClientWrapper.cs
@@ -11,7 +11,7 @@ namespace PocketBase.Net.Client;
 /// <summary>
 /// PocketBase HTTP client wrapper that manages its authentication status, default endpoints and serialization.
 /// </summary>
-/// <param name="configuration">The initial configuration, seeding the dependencie's options.</param>
+/// <param name="configuration">The initial configuration, seeding the dependencies options.</param>
 public sealed class PocketBaseHttpClientWrapper(PocketBaseClientConfiguration configuration)
 {
     /// <summary>
@@ -76,108 +76,6 @@ public sealed class PocketBaseHttpClientWrapper(PocketBaseClientConfiguration co
     }
 
     /// <summary>
-    /// Send a POST request to a collection to create a new record.
-    /// </summary>
-    /// <typeparam name="TPayload">The type of the payload to send in the body of the POST request.</typeparam>
-    /// <typeparam name="TRecord">The type of the expected response.</typeparam>
-    /// <param name="collectionIdOrName">The collection id or name in which the record will be created.</param>
-    /// <param name="payload">The content to be serialized as the body of the POST request.</param>
-    /// <param name="cancellationToken">A cancellation token.</param>
-    /// <returns>The newly created <typeparamref name="TRecord"/> on success.</returns>
-    public async Task<TRecord> SendPost<TPayload, TRecord>(
-        string collectionIdOrName,
-        TPayload payload,
-        CancellationToken cancellationToken
-    ) where TRecord : RecordBase
-    {
-        if (!IsAuthenticated)
-        {
-            await HandleUnauthenticatedClient(cancellationToken);
-        }
-
-        using var response = await _httpClient.PostAsJsonAsync(
-           $"/api/collections/{collectionIdOrName}/records",
-           payload,
-           _jsonSerializerOptions,
-           cancellationToken);
-
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new RecordCreationFailedException
-            {
-                Payload = payload,
-                RequestMessage = response.RequestMessage!,
-                Response = response.Content,
-            };
-        }
-
-        var rawContent = await response.Content.ReadAsStringAsync(cancellationToken);
-
-        return JsonSerializer.Deserialize<TRecord>(rawContent, _jsonSerializerOptions)
-            ?? throw new MalformedResponseException<TRecord> { Received = rawContent };
-    }
-
-    /// <summary>
-    /// Send a DELETE request to a collection to delete an existing record.
-    /// </summary>
-    /// <param name="collectionIdOrName">The collection id or name in which the record will be deleted.</param>
-    /// <param name="recordId">The id of the record to delete..</param>
-    /// <param name="cancellationToken">A cancellation token.</param>
-    public async Task SendDelete(
-        string collectionIdOrName,
-        string recordId,
-        CancellationToken cancellationToken)
-    {
-        if (!IsAuthenticated)
-        {
-            await HandleUnauthenticatedClient(cancellationToken);
-        }
-
-        using var response = await _httpClient.DeleteAsync(
-            $"/api/collections/{collectionIdOrName}/records/{recordId}",
-            cancellationToken);
-
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new RecordDeletionFailedException
-            {
-                RecordId = recordId,
-                RequestMessage = response.RequestMessage!,
-                Response = response.Content,
-            };
-        }
-    }
-
-    /// <summary>
-    /// Send a GET request to a collection to retrive an existing record
-    /// </summary>
-    /// <typeparam name="TRecord">The type of the expected response.</typeparam>
-    /// <param name="collectionIdOrName">The collection id or name in which the record is.</param>
-    /// <param name="recordId">The id of the record to retrieve.</param>
-    /// <param name="cancellationToken">A cancellation token.</param>
-    /// <returns>The retrieved <typeparamref name="TRecord"/>.</returns>
-    public async Task<TRecord> SendGet<TRecord>(
-        string collectionIdOrName,
-        string recordId,
-        CancellationToken cancellationToken
-    )
-    {
-        using var response = await _httpClient.GetAsync(
-            $"/api/collections/{collectionIdOrName}/records/{recordId}",
-            cancellationToken);
-
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new RecordSearchFailedException();
-        }
-
-        var rawContent = await response.Content.ReadAsStringAsync(cancellationToken);
-
-        return JsonSerializer.Deserialize<TRecord>(rawContent, _jsonSerializerOptions)
-            ?? throw new MalformedResponseException<TRecord> { Received = rawContent };
-    }
-
-    /// <summary>
     /// Attempt to authenticate the client according to the configuration behavior.
     /// </summary>
     /// <param name="cancellationToken">A cancellation token.</param>
@@ -189,6 +87,47 @@ public sealed class PocketBaseHttpClientWrapper(PocketBaseClientConfiguration co
             : throw new UnauthenticatedClientException();
 
     /// <summary>
+    /// Send a DELETE request to a collection to delete an existing record.
+    /// </summary>
+    /// <param name="collectionIdOrName">The collection id or name in which the record will be deleted.</param>
+    /// <param name="recordId">The id of the record to delete..</param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    public Task SendDelete(
+        string collectionIdOrName,
+        string recordId,
+        CancellationToken cancellationToken)
+        => SendRequest(
+                (httpClient) => httpClient.DeleteAsync(
+                    $"/api/collections/{collectionIdOrName}/records/{recordId}",
+                    cancellationToken),
+                onErrorThrow: (response) => new RecordDeletionFailedException
+                {
+                    RecordId = recordId,
+                    RequestMessage = response.RequestMessage!,
+                    Response = response.Content,
+                }, cancellationToken);
+
+    /// <summary>
+    /// Send a GET request to a collection to retrieve an existing record
+    /// </summary>
+    /// <typeparam name="TRecord">The type of the expected response.</typeparam>
+    /// <param name="collectionIdOrName">The collection id or name in which the record is.</param>
+    /// <param name="recordId">The id of the record to retrieve.</param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>The retrieved <typeparamref name="TRecord"/>.</returns>
+    public Task<TRecord> SendGet<TRecord>(
+        string collectionIdOrName,
+        string recordId,
+        CancellationToken cancellationToken
+     ) where TRecord : RecordBase
+        => SendRequest<TRecord>(
+            (httpClient) => httpClient.GetAsync(
+                $"/api/collections/{collectionIdOrName}/records/{recordId}",
+                cancellationToken),
+            onErrorThrow: (_) => new RecordSearchFailedException(),
+            cancellationToken);
+
+    /// <summary>
     /// Send a PATCH request to a collection to update an existing record
     /// </summary>
     /// <typeparam name="TRecord">The type of the expected response.</typeparam>
@@ -197,38 +136,109 @@ public sealed class PocketBaseHttpClientWrapper(PocketBaseClientConfiguration co
     /// <param name="payload">The content to be serialized as the body of the PATCH request.</param>
     /// <param name="cancellationToken">A cancellation token.</param>
     /// <returns>The updated <typeparamref name="TRecord"/>.</returns>
-    public async Task<TRecord> SendPatch<TRecord>(
+    public Task<TRecord> SendPatch<TRecord>(
         string collectionIdOrName,
         string recordId,
         IDictionary<string, object?> payload,
         CancellationToken cancellationToken
     ) where TRecord : RecordBase
-    {
-        if (!IsAuthenticated)
-        {
-            await HandleUnauthenticatedClient(cancellationToken);
-        }
-
-        using var response = await _httpClient.PatchAsJsonAsync(
-           $"/api/collections/{collectionIdOrName}/records/{recordId}",
-           payload,
-           _jsonSerializerOptions,
-           cancellationToken);
-
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new RecordUpdateFailedException
+        => SendRequest<TRecord>(
+            (httpClient) => httpClient.PatchAsJsonAsync(
+               $"/api/collections/{collectionIdOrName}/records/{recordId}",
+               payload,
+               _jsonSerializerOptions,
+               cancellationToken
+            ),
+            onErrorThrow: (response) => new RecordUpdateFailedException
             {
                 RecordId = recordId,
                 Payload = payload,
                 RequestMessage = response.RequestMessage!,
                 Response = response.Content,
-            };
+            },
+            cancellationToken);
+
+    /// <summary>
+    /// Send a POST request to a collection to create a new record.
+    /// </summary>
+    /// <typeparam name="TPayload">The type of the payload to send in the body of the POST request.</typeparam>
+    /// <typeparam name="TRecord">The type of the expected response.</typeparam>
+    /// <param name="collectionIdOrName">The collection id or name in which the record will be created.</param>
+    /// <param name="payload">The content to be serialized as the body of the POST request.</param>
+    /// <param name="cancellationToken">A cancellation token.</param>
+    /// <returns>The newly created <typeparamref name="TRecord"/> on success.</returns>
+    public Task<TRecord> SendPost<TPayload, TRecord>(
+        string collectionIdOrName,
+        TPayload payload,
+        CancellationToken cancellationToken
+    ) where TRecord : RecordBase
+        => SendRequest<TRecord>(
+            (httpClient) => httpClient.PostAsJsonAsync(
+               $"/api/collections/{collectionIdOrName}/records",
+               payload,
+               _jsonSerializerOptions,
+               cancellationToken),
+            onErrorThrow: (response) => new RecordCreationFailedException
+            {
+                Payload = payload,
+                RequestMessage = response.RequestMessage!,
+                Response = response.Content,
+            },
+            cancellationToken);
+
+    /// <summary>
+    /// Sends an HTTP request, ensuring the client is authenticated, and handles error responses.
+    /// </summary>
+    /// <param name="request">A delegate that takes an <see cref="HttpClient"/> and returns a task representing the HTTP response message.</param>
+    /// <param name="onErrorThrow">A delegate that takes the failed <see cref="HttpResponseMessage"/> and returns an appropriate <see cref="PocketBaseException"/> to throw.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    /// <remarks>
+    /// If the client is not authenticated, it will attempt to authenticate based on the configuration before sending the request.
+    /// </remarks>
+    private async Task SendRequest(
+        Func<HttpClient, Task<HttpResponseMessage>> request,
+        Func<HttpResponseMessage, PocketBaseException> onErrorThrow,
+        CancellationToken cancellationToken)
+    {
+        if (!IsAuthenticated)
+        {
+            await HandleUnauthenticatedClient(cancellationToken);
         }
+        using var response = await request.Invoke(_httpClient);
+        if (!response.IsSuccessStatusCode)
+        {
+            throw onErrorThrow.Invoke(response);
+        }
+    }
 
+    /// <summary>
+    /// Sends an HTTP request, ensures the client is authenticated, handles error responses, and deserializes the response content.
+    /// </summary>
+    /// <typeparam name="TDeserialized">The type of the expected response after deserialization.</typeparam>
+    /// <param name="request">A delegate that takes an <see cref="HttpClient"/> and returns a task representing the HTTP response message.</param>
+    /// <param name="onErrorThrow">A delegate that takes the failed <see cref="HttpResponseMessage"/> and returns an appropriate <see cref="PocketBaseException"/> to throw.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation, containing the deserialized response of type <typeparamref name="TDeserialized"/>.</returns>
+    /// <remarks>
+    /// If the client is not authenticated, it will attempt to authenticate based on the configuration before sending the request.
+    /// </remarks>
+    private async Task<TDeserialized> SendRequest<TDeserialized>(
+        Func<HttpClient, Task<HttpResponseMessage>> request,
+        Func<HttpResponseMessage, PocketBaseException> onErrorThrow,
+        CancellationToken cancellationToken)
+    {
+        if (!IsAuthenticated)
+        {
+            await HandleUnauthenticatedClient(cancellationToken);
+        }
+        using var response = await request.Invoke(_httpClient);
+        if (!response.IsSuccessStatusCode)
+        {
+            throw onErrorThrow.Invoke(response);
+        }
         var rawContent = await response.Content.ReadAsStringAsync(cancellationToken);
-
-        return JsonSerializer.Deserialize<TRecord>(rawContent, _jsonSerializerOptions)
-            ?? throw new MalformedResponseException<TRecord> { Received = rawContent };
+        return JsonSerializer.Deserialize<TDeserialized>(rawContent, _jsonSerializerOptions)
+            ?? throw new MalformedResponseException<TDeserialized> { Received = rawContent };
     }
 }
